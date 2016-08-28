dfee43ac98109364b1629a6b42c7acce94f54aad
Luchogar <luis_ign@yahoo.com.mx>
2016-08-28 10:44:09 -0500

    review

--- app/src/main/java/mx/grupogarcia/coursera/datos.java
+++ app/src/main/java/mx/grupogarcia/coursera/datos.java
@@ -4,6 +4,7 @@ package mx.grupogarcia.coursera;
  * Created by Luis García on 27/08/2016.
  */
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,9 +15,54 @@ import android.widget.TextView;
 
 
 public class datos extends ActionBarActivity {
+    personal persona;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+
+        Bundle bundle = getIntent().getExtras();
+
+        TextView tvNombreCompleto = (TextView) findViewById(R.id.muestra1);
+        TextView tvTelefono = (TextView) findViewById(R.id.muestra2);
+        TextView tvCorreo = (TextView) findViewById(R.id.muestra3);
+        TextView tvDescripcion = (TextView) findViewById(R.id.muestra4);
+        TextView tvFechaNacimiento = (TextView) findViewById(R.id.muestra5);
+
+        Button btnEditarDatos = (Button) findViewById(R.id.edit);
+        persona = new personal();
+
+        persona.setNombreCompleto(bundle.getString(getResources().getString(R.string.nombre)));
+        persona.setTelefono(bundle.getString(getResources().getString(R.string.telefono)));
+        persona.setCorreo(bundle.getString(getResources().getString(R.string.email)));
+        persona.setDescripcion(bundle.getString(getResources().getString(R.string.descrip)));
+        persona.setDia(bundle.getInt(getResources().getString(R.string.DIA)));
+        persona.setMes(bundle.getInt(getResources().getString(R.string.MES)));
+        persona.setAño(bundle.getInt(getResources().getString(R.string.AÑO)));
+
+        tvNombreCompleto.setText(persona.getNombreCompleto());
+        tvFechaNacimiento.setText(persona.getFechaNacimiento());
+        tvTelefono.setText(persona.getTelefono());
+        tvCorreo.setText(persona.getCorreo());
+        tvDescripcion.setText(persona.getDescripcion());
+
+        btnEditarDatos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(datos.this, manejo.class);
+                intent.putExtra(getResources().getString(R.string.nombre), persona.getNombreCompleto());
+                intent.putExtra(getResources().getString(R.string.telefono), persona.getTelefono());
+                intent.putExtra(getResources().getString(R.string.email), persona.getCorreo());
+                intent.putExtra(getResources().getString(R.string.descrip), persona.getDescripcion());
+                intent.putExtra(getResources().getString(R.string.DIA), persona.getDia());
+                intent.putExtra(getResources().getString(R.string.MES), persona.getMes());
+                intent.putExtra(getResources().getString(R.string.AÑO), persona.getAño());
+                startActivity(intent);
+                finish();
+            }
+        });
+
 
     }
 }
--- app/src/main/java/mx/grupogarcia/coursera/manejo.java
+++ app/src/main/java/mx/grupogarcia/coursera/manejo.java
@@ -5,36 +5,79 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 /**
  * Created by Luis García on 27/08/2016.
  */
 public class manejo extends ActionBarActivity {
+
+    private String nombre;
+    private String telefono;
+    private String email;
+    private String descrpcion;
+    private DatePicker Fech;
+
+    private personal persona;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button boton= (Button)findViewById(R.id.siguiente);
-        boton.setOnClickListener(new View.OnClickListener() {
+
+       // Button boton= (Button)findViewById(R.id.siguiente);
+       // boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+           // public void onClick(View view) {
+
+                    Button next;
+
+        nombre          = (TextInputEditText) findViewById(R.id.name);
+        Fech            = (DatePicker) findViewById(R.id.Fech);
+        telefono        = (TextInputEditText) findViewById(R.id.telefono);
+        email           = (TextInputEditText) findViewById(R.id.correo);
+        descrpcion      = (TextInputEditText) findViewById(R.id.description);
+        next            = (Button) findViewById(R.id.siguiente);
+
+
+        persona = new personal();
+
+        Bundle bundle = getIntent().getExtras();
+
+
+        next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String nombre       = ((EditText)findViewById(R.id.name)).getText().toString();
-                String telefono     = ((EditText)findViewById(R.id.telefono)).getText().toString();
-                String email        = ((EditText)findViewById(R.id.correo)).getText().toString();
-                String descripcion  = ((EditText)findViewById(R.id.description)).getText().toString();
+                Intent intent = new Intent(manejo.this, datos.class);
+                LeerDatos();
+                intent.putExtra(getResources().getString(R.string.nombre), persona.getNombreCompleto());
+                intent.putExtra(getResources().getString(R.string.telefono), persona.getTelefono());
+                intent.putExtra(getResources().getString(R.string.email), persona.getCorreo());
+                intent.putExtra(getResources().getString(R.string.descrip), persona.getDescripcion());
+                intent.putExtra(getResources().getString(R.string.DIA), persona.getDia());
+                intent.putExtra(getResources().getString(R.string.MES), persona.getMes());
+                intent.putExtra(getResources().getString(R.string.AÑO), persona.getAño());
 
-                Intent nueva = new Intent(manejo.this,datos.class);
-
-                startActivity(nueva);
-
-
-
-
-
-
+                startActivity(intent);
+                finish();
             }
         });
+
+
+
+            //}
+        });
+    }
+    private void LeerDatos(){
+        persona.setNombreCompleto(textnombre.getText().toString());
+        persona.setTelefono(texttelefono.getText().toString());
+        persona.setCorreo(textemail.getText().toString());
+        persona.setDescripcion(textdescrip.getText().toString());
+        persona.setDia(Fech.getDayOfMonth());
+        persona.setMes(Fech.getMonth());
+        persona.setAño(Fech.getYear());
     }
 }
